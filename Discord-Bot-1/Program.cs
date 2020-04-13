4e0742b5a9b5008496036705e8b66d8d3a761081
typicalname500 <48390978+typicalname500@users.noreply.github.com>
2020-04-13 02:21:12 +0100

    Setup of c# version

---
+++ Discord-Bot-1/Program.cs
@@ -0,0 +1,239 @@
+﻿//###################################################################################
+//#Using the brilliant API that I could only hope to call my own
+//#>>>>>>>>>>>>>>>>https://fifteen.ai/<<<<<<<<<<<<<<<<<<<<
+//###################################################################################
+
+using System;
+using Discord;
+using Discord.Net;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+//using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace DiscordBot1
+{
+
+    public class Request
+    {
+        //ew ew ew ew ew ew ew                                                   \/
+        //Should be something else but I forget what it should be :thonk:
+        //Currently 4 by 13
+        //hey x say y
+        public string[,] Characterinfo =  {
+        {"GLaDOS","hey glados, say" , "0"},
+        {"Twilight Sparkle","hey twi, say" , "0"},{"Twilight Sparkle","hey purps, say" , "0"},{"Twilight Sparkle","hey twilight, say" , "0"},{"Twilight Sparkle","hey twiggles, say" , "0"},{"Twilight Sparkle","hey twiggle piggle, say", "0"},
+        {"Wheatley","hey wheatley, say" , "0"},
+        {"The Narrator","hey narrator, say" , "0"},
+        {"Tenth Doctor","hey doc, say" , "0"},{"Tenth Doctor","hey doctor, say" , "0"},
+        {"Soldier","hey soldier, say" , "0"},{"Soldier","hey soli, say" , "0"},
+        {"Sans","hey sans, say" , "0"},
+        {"Fluttershy","hey fluttershy, say" , "0"},{"Fluttershy","hey shy, say" , "0"},{"Fluttershy","hey flutters, say" , "0"},{"Fluttershy","hey flutterbutter, say" , "0"},{"Fluttershy","hey fluttershush, say" , "0"},{"Fluttershy","hey butter shush, say", "0"},{"Fluttershy","hey flutter butter, say" , "0"},{"Fluttershy","hey flutter shush, say" , "0"},{"Fluttershy","hey butter shush, say", "0"},
+        {"Rarity","hey rarity, say" , "0"},{"Rarity","hey darling, say" , "0"},{"Rarity","hey white ranger, say" , "0"},
+        {"Applejack","hey applejack, say" , "0"},{"Applejack","hey apples, say" , "0"},{"Applejack","hey applez, say" , "0"},{"Applejack","hey jackapple, say" , "0"},{"Applejack","hey aj, say", "0"},
+        {"Rainbow Dash","hey rainbow dash, say" , "0"},{"Rainbow Dash","hey dash, say" , "0"},{"Rainbow Dash","hey dashie, say" , "0"},{"Rainbow Dash","hey rd, say" , "0"},
+        {"Pinkie Pie","hey pinkie pie, say" , "0"},{"Pinkie Pie","hey pinkie, say" , "0"},{"Pinkie Pie","hey ponka pie, say" , "0"},{"Pinkie Pie","hey pinker ponk, say", "0"},
+        {"Princess Celestia","hey princess celestia, say" , "0"},{"Princess Celestia","hey princess, say" , "0"},{"Princess Celestia","hey sunbutt, say" , "0"},{"Princess Celestia","hey sun butt, say" , "0"},{"Princess Celestia","hey tia, say" , "0"},{"Princess Celestia","tia, say", "1"},
+        //"Tell me about"
+        {"GlaDOS","hey glados, tell me about", "1"},
+        {"Twilight Sparkle","hey twi, tell me about", "1"},{"Twilight Sparkle","hey purps, tell me about", "1"},{"Twilight Sparkle","hey twilight, tell me about", "1"},{"Twilight Sparkle","hey twiggles, tell me about", "1"},{"Twilight Sparkle","hey twiggle piggle, tell me about", "1"},
+        {"Wheatley","hey wheatley, tell me about", "1"},
+        {"The Narrator","hey narrator, tell me about", "1"},
+        {"Tenth Doctor","hey doc, tell me about", "1"},{"Tenth Doctor","hey doctor, tell me about", "1"},
+        {"Soldier","hey soldier, tell me about", "1"},{"Soldier","hey soli, tell me about", "1"},
+        {"Sans","hey sans, tell me about", "1"},
+        {"Fluttershy","hey fluttershy, tell me about", "1"},{"Fluttershy","hey shy, tell me about", "1"},{"Fluttershy","hey flutters, tell me about", "1"},{"Fluttershy","hey flutterbutter, tell me about", "1"},{"Fluttershy","hey fluttershush, tell me about", "1"},{"Fluttershy","hey butter shush, tell me about", "1"},{"Fluttershy","hey flutter butter, tell me about", "1"},{"Fluttershy","hey flutter shush, tell me about", "1"},{"Fluttershy","hey butter shush, tell me about", "1"},
+        {"Rarity","hey rarity, tell me about", "1"},{"Rarity","hey darling, tell me about", "1"},{"Rarity","hey white ranger, tell me about", "1"},
+        {"Applejack","hey applejack, tell me about", "1"},{"Applejack","hey apples, tell me about", "1"},{"Applejack","hey applez, tell me about", "1"},{"Applejack","hey jackapple, tell me about", "1"},{"Applejack","hey aj, tell me about", "1"},
+        {"Rainbow Dash","hey rainbow dash, tell me about", "1"},{"Rainbow Dash","hey dash, tell me about", "1"},{"Rainbow Dash","hey dashie, tell me about", "1"},{"Rainbow Dash","hey rd, tell me about", "1"},
+        {"Pinkie Pie","hey pinkie pie, tell me about", "1"},{"Pinkie Pie","hey pinkie, tell me about", "1"},{"Pinkie Pie","hey ponka pie, tell me about", "1"},{"Pinkie Pie","hey pinker ponk, tell me about", "1"},
+        {"Princess Celestia","hey princess celestia, tell me about", "1"},{"Princess Celestia","hey princess, tell me about", "1"},{"Princess Celestia","hey sunbutt, tell me about", "1"},{"Princess Celestia","hey sun butt, tell me about", "1"},{"Princess Celestia","hey tia, tell me about", "1"},{"Princess Celestia","tia, tell me about", "1"}
+        };
+
+
+        //Making a request and responding accordingly
+        public static async Task MakerequestAsync(string character, string basemessage, SocketMessage message)
+        {
+            //Declaring handler
+            var handler = new HttpClientHandler();
+
+            //Keeping just in case the wavs are zipped with gzip n' co
+            //handler.AutomaticDecompression = ~DecompressionMethods.None;
+
+
+            using (var httpClient = new HttpClient(handler))
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.fifteen.ai/app/getAudioFile"))
+                {
+                    //request.Headers.TryAddWithoutValidation("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Mobile Safari/537.36");
+                    request.Headers.TryAddWithoutValidation("user-agent", "FifteenCLI");
+                    request.Content = new StringContent("{\"text\":\"" + basemessage + "\",\"character\":\"" + character + "\"}");
+                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json;charset=UTF-8");
+                    var response = await httpClient.SendAsync(request);
+
+                    if (response.StatusCode.ToString() != "OK")
+                    {
+                        //Responding to error msg given
+                        Console.WriteLine(response.StatusCode.ToString());
+
+                        //Waiting 10 seconds (There are better methods of waiting ik but a 1 liner suits me for now)
+                        Console.WriteLine("Will send again");
+                        Thread.Sleep(10000);
+                        
+                        //Sending the request again
+                        Console.WriteLine("Sending");
+                        _ = MakerequestAsync(character, basemessage, message);
+
+                    }
+                    else
+                    {
+                        //Responding with OK
+                        Console.WriteLine(response.StatusCode.ToString());
+
+                        //Storing locally and posting from local because reasons (will be set to variable to suit > 75 char strings given)
+                        byte[] data = await response.Content.ReadAsByteArrayAsync();
+                        System.IO.File.WriteAllBytes("C:\\Users\\Tom\\Documents\\Projectz\\AI API\\test.wav", data);
+                        await message.Channel.SendFileAsync("C:\\Users\\Tom\\Documents\\Projectz\\AI API\\test.wav", "Test");
+                    }
+                }
+            }
+        }
+    }
+
+
+    class Program
+    {
+        //Discord stuff (shamelessly robbed from https://github.com/gngrninja/csharpi/tree/intro)
+        private readonly DiscordSocketClient _client;
+        private readonly IConfiguration _config;
+
+        //Starting the program
+        static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public Program()
+        {
+            _client = new DiscordSocketClient();
+
+            //Hook into log event and write it out to the console
+            _client.Log += LogAsync;
+
+            //Hook into the client ready event
+            _client.Ready += ReadyAsync;
+
+            //Hook into the message received event, this is how we handle the hello world example
+            _client.MessageReceived += MessageReceivedAsync;
+
+            //Setting config stuff including token
+            var _builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(path: "config.json");
+            _config = _builder.Build();
+        }
+
+        public async Task MainAsync()
+        {
+            //This is where we get the Token value from the configuration file
+            await _client.LoginAsync(TokenType.Bot, _config["Token"]);
+            await _client.StartAsync();
+
+            // Block the program until it is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"Connected");
+            return Task.CompletedTask;
+        }
+
+        public async Task<List<string>> SplitbasestringAsync(string basemessage)
+        {
+            List<string> messagelist = new List<string>();
+            int chunkSize = 73;
+            int stringLength = basemessage.Length;
+            for (int i = 0; i < stringLength; i += chunkSize)
+            {
+                if (i + chunkSize > stringLength) chunkSize = stringLength - i;
+                Console.WriteLine(basemessage.Substring(i, chunkSize));
+                messagelist.Add(basemessage.Substring(i, chunkSize) + ".");             
+            }
+            return messagelist;
+        }
+
+        
+
+        //There's a better way of handling this as highlighted in the git repo but eh 
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            //Calling mah class
+            Request requestclass = new Request();
+
+            //This the bot doesn't respond to itself
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;        
+        
+
+            //Spaz tier method of extracting and acting on message given
+            for (int i = 0; i < 86;i++)
+            {
+                //Checking the message if it fits
+                if (message.Content.Contains(requestclass.Characterinfo[i, 1]))
+                {
+                    //Checking (Response type e.g hey x say y or tell me about, that meant to return wiki paragraph sections)
+                    if (requestclass.Characterinfo[i,2] == "0")
+                    {                     
+                        //Stripping string of "hey x, say" and adding a . (all requests need it/ is the reason on the fifteen.ai/app page the counter goes to 1 instead of 0)
+                        //In addition, for some reason simply using "substring(x,y) + "." "isn't liked by this
+                        string parsedmessage = requestclass.Characterinfo[i, 1] + " ";
+                        string basemessage = message.Content.Replace(parsedmessage,"");
+                        basemessage = basemessage + ".";
+
+                        if (basemessage.Length <= 75)
+                        {                          
+                            //Making the request and showing it in console
+                            Console.WriteLine(requestclass.Characterinfo[i, 0] + " says : " + basemessage);
+                            _ = Request.MakerequestAsync(requestclass.Characterinfo[i, 0], basemessage, message);
+                        }
+                        else
+                        {
+                            List<string> messagelist = new List<string>();
+                            messagelist = await SplitbasestringAsync(basemessage);                           
+                            foreach (string listitem in messagelist)
+                            {                              
+                                Console.WriteLine(requestclass.Characterinfo[i, 0] + " says : " + listitem);
+                                _ = Request.MakerequestAsync(requestclass.Characterinfo[i, 0], listitem, message);
+                            }
+                        }
+                                           
+                        //Debug message to be uncommented if needed
+                        //await message.Channel.SendMessageAsync(requestclass.Characterinfo[i, 0] + "  " + requestclass.Characterinfo[i, 1] + "  " + requestclass.Characterinfo[i, 2]);                     
+                    }
+                    else if (requestclass.Characterinfo[i,2] == "1")
+                    {
+                        //Message sent to wiki then responded with x saying the paragraph in the wiki (may need to look into further parsing however as some things may not be as precise with what I want)
+                        
+                    }
+                    
+                }
+            }
+        }
+    }
+}
